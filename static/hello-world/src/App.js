5144e9efff17f39201092a50d298cf7949c71fa9
Peter Velosy <peter@petervelosy.com>
2024-01-11 20:41:39 +0100

    Working example of the AtlasKit editor

--- static/hello-world/src/App.js
+++ static/hello-world/src/App.js
@@ -1,20 +1,44 @@
 import React, { useEffect, useState } from 'react';
 import { invoke } from '@forge/bridge';
-import {Editor} from "@atlaskit/editor-core";
+import { EditorContext, WithEditorActions } from '@atlaskit/editor-core';
+import { ComposableEditor } from '@atlaskit/editor-core/composable-editor';
+import { createDefaultPreset } from '@atlaskit/editor-core/preset-default';
 
-function App() {
+const App = () => {
   const [data, setData] = useState(null);
 
   useEffect(() => {
     invoke('getText', { example: 'my-invoke-variable' }).then(setData);
   }, []);
 
+  const presetProps = {
+    props: {},
+  };
+  const defaultPreset = createDefaultPreset(presetProps);
+  const initialADFDocument = {
+    version: 1,
+    type: 'doc',
+    content: [
+      {
+        type: 'paragraph',
+        content: [{ type: 'text', text: 'Hello World!' }],
+      },
+    ],
+  };
+
   return (
-    <div>
-      {data ? data : 'Loading...'}
-      <Editor/>
-    </div>
+      <EditorContext>
+        <WithEditorActions
+            render={(editorActions) => (
+                <ComposableEditor
+                    preset={defaultPreset}
+                    defaultValue={initialADFDocument}
+                    onChange={async () => console.log('The current doc is: ', await editorActions.getValue())}
+                />
+            )}
+        />
+      </EditorContext>
   );
-}
+};
 
 export default App;
